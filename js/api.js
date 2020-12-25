b5f61a5625d50afe8224e6a3639d1ea9f91814f9
wangjiaozhu <183397156@qq.com>
2020-12-25 17:26:24 +0800

    update

--- js/api.js
+++ js/api.js
@@ -3,5 +3,20 @@ var BaseUrl = 'https://www.sidama.work/'
 api = {
     // 获取商品列表
     shopList: BaseUrl + 'goods/page',
-    shopDetails: BaseUrl + 'goods/'
+    shopDetails: BaseUrl + 'goods/',
+    sendMsg: BaseUrl + 'user/sendMs',
+    regeister: BaseUrl + 'user/register',
+    addGoodsCart: BaseUrl + 'cart/save',
+    goodsCartList: BaseUrl + 'cart/page',
+    goodsCartDel: BaseUrl + 'cart/delete',
+    team: BaseUrl + 'userManager/team',
+    userInfo: BaseUrl + 'userManager/userInfo',
+    addressList: BaseUrl + 'address/getMyAddress',
+    addAddress: BaseUrl + 'address/saveMyAddress',
+    updateAddress: BaseUrl + 'address/updateMyAddress',
+    addressDelete: BaseUrl + 'address/delete',
+    bankInfo: BaseUrl + 'userManager/getSelfAccount',
+    saveBankInfo: BaseUrl + 'userManager/updateSelfAccount',
+    personalInfo: BaseUrl + 'userManager/getIdentity',
+    savePersonalInfo: BaseUrl + 'userManager/updateIdentity',
 }
